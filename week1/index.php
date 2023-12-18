d4ac2d609af10ed197d9154395da43fc63c21121
Supavit Dechpipatttinnachot <154107514+Supavit-Dechpipattinnachot@users.noreply.github.com>
2023-12-18 11:09:02 +0700

    Add files via upload

---
+++ week1/index.php
@@ -0,0 +1,16 @@
+<?php
+    $address = "homeless";
+    $char1 = 'A';
+    $age = 147 ;
+    $p1 = 3.14;
+    $p1 = 3.14144524;
+    //
+    echo $address[0].$address[1].$address[2].$address[3];
+    echo "<br>";
+    echo $char1 ;
+    echo "<br>";
+    echo $age+$p1;
+
+    $isTrue = true;
+    echo $isTrue;
+?>
